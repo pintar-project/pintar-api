6400545edd8cff88f1fb85456df8fc295e09dd85
eldovyn <farras.pramudita@gmail.com>
2026-03-25 13:55:20 +0700

    feat: update config docker

--- core/config.py
+++ core/config.py
@@ -5,9 +5,11 @@ class Settings(BaseSettings):
     CLOUDINARY_CLOUD_NAME: str
     CLOUDINARY_API_KEY: str
     CLOUDINARY_API_SECRET: str
+    API_PORT: int = 8000
 
     class Config:
         env_file = ".env"
+        extra = "ignore"
 
 
 settings = Settings()
